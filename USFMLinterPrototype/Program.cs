706762a990e9660ae6acddbfd1e056e8b568b155
Craig Oliver <craig.robert.oliver@gmail.com>
2023-01-04 14:55:11 -0500

    Starting project

---
+++ USFMLinterPrototype/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace USFMLinterPrototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
